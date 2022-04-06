235c43e98627cb515931b368837ae1a1bedbe85d
haeji08 <83302507+haeji08@users.noreply.github.com>
2022-04-06 22:21:25 +0900

    섹션2,3,4

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+//    @Autowired private  MemberService memberService;  //필드주입
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
