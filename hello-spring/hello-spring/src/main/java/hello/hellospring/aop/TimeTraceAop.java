b6a6298b4a1cbc88ae1991de4fd02181bc57c3b1
haeji08 <83302507+haeji08@users.noreply.github.com>
2022-05-03 07:07:48 +0900

    섹션 7,8

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());  // 어떤 메소드를 call하는지 찍을 수 있음
+        try {
+            return joinPoint.proceed();  //다음 메소드로 진행이 됨
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+
+    }
+}
